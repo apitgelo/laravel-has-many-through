1d6c0dc7cb0100eb3b04075cb29e8a565a430a89
Hafidz Prasetya <apitgelo@gmail.com>
2018-09-26 18:25:28 +0700

    Define relationship in User

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function goals()
+    {
+        return $this->hasMany(Goal::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
