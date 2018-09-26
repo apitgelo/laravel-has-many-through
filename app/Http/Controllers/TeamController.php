8e0dc12212ed0ab293b4e106c5b175667241ed8e
Hafidz Prasetya <apitgelo@gmail.com>
2018-09-26 18:32:53 +0700

    Create method to show team and add new route

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Team;
 
 class TeamController extends Controller
 {
-    //
+    public function show(Team $team)
+    {
+        dd($team);
+    }
 }
