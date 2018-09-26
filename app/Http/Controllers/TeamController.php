1a219285efb62f36c9625e3bec326f4526be3d5e
Hafidz Prasetya <apitgelo@gmail.com>
2018-09-26 18:36:37 +0700

    Change method

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -8,6 +8,8 @@ class TeamController extends Controller
 {
     public function show(Team $team)
     {
-        dd($team);
+        $goals = $team->goals()->get();
+
+        return view('team.show', compact('goals'));
     }
 }
